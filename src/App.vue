9ae9897dfa1b09a22d8b3fa3f97e5161e7d63e23
yycinm <yycinm@hanmail.net>
2022-10-13 16:07:56 +0900

    aaa

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  
+  <div class="container">
+    ddddd
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,8 @@ export default {
 </script>
 
 <style>
-
+.container{
+    width: 100px;height: 300px;
+    background: #000;
+}
 </style>
