856e44eb3998a7a58446680a082d3722270ae343
yycinm <yycinm@hanmail.net>
2022-11-04 15:30:56 +0900

    ss

--- src/components/VisualSubscribe.vue
+++ src/components/VisualSubscribe.vue
@@ -1,13 +1,44 @@
 <template>
-  
+  <div class="container">
+    <div class="subTxt">
+
+    <h2>JA Korea 뉴스레터 구독하기</h2>
+    <div class="sear">
+      <form action="#">
+        <input type="text" name="search" placeholder="이름" class="inp" />
+        <div class="icon">
+          <i class="fa-solid fa-user"></i>
+        </div>
+      </form>
+      <form action="#">
+        <input
+          type="text"
+          name="search"
+          placeholder="이메일 주소"
+          class="inp"
+        />
+        <div class="icon">
+          <i class="fa-solid fa-envelope"></i>
+        </div>
+      </form>
+      <div class="sbtn">
+        <h5>신청하기</h5>
+      </div>
+    </div>
+    <div class="sAgree">
+        <i class="fa-regular fa-circle-check"></i>
+        <h6>뉴스레터 구독을 위한 이름 및 이메일 주소 수집에 동의합니다.</h6>
+    </div>
+    </div>
+    <div class="simg">
+        <img src="" alt="">
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
